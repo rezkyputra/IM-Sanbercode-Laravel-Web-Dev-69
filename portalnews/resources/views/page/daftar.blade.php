5a3687433740917159f6f7de99f546513a07d4e8
rezkyputra <rezky.putra46@gmail.com>
2025-08-20 17:34:03 +0800

    latihan templating

--- portalnews/resources/views/page/daftar.blade.php
+++ portalnews/resources/views/page/daftar.blade.php
@@ -1,21 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Halaman Pendaftaran</h1>
-    <form action="/kirim" method="POST">
-        @csrf
-        <label >Full Name</label> <br>
-        <input type="text" name="fullname" > <br> <br>
-        <label >Usia</label> <br>
-        <input type="number" name="age"> <br> <br>
+@extends('layouts.master')
 
-        <input type="submit" value="daftar">
-    </form>
-</body>
-</html>
+@section('title')
+    Halaman Pendaftaran
+@endsection
+@section('content')
+    
+<form action="/kirim" method="POST">
+    @csrf
+    <label >Full Name</label> <br>
+    <input type="text" name="fullname" > <br> <br>
+    <label >Usia</label> <br>
+    <input type="number" name="age"> <br> <br>
+
+    <input type="submit" value="daftar">
+</form>
+@endsection
--- portalnews/resources/views/welcome.blade.php
+++ portalnews/resources/views/welcome.blade.php
@@ -1,18 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Halaman Utama</h1>
-    <ul>
-        <li>List 1</li>
-        <li>List 2 <a href="/daftar">Daftar Disini</a> </li>
-        <li>List 3</li>
-        <li>List 4</li>
-    </ul>
-</body>
-</html>
+@extends('layouts.master')
+
+@section('title')
+    Halaman Utama
+@endsection
+@section('content')
+    
+<ul>
+    <li>List 1</li>
+    <li>List 2 <a href="/daftar">Daftar Disini</a> </li>
+    <li>List 3</li>
+    <li>List 4</li>
+</ul>
+@endsection
